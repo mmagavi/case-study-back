9a1f4c111834a658273204c62f12962d2cb664df
Maya Magavi <maya_magavi@brown.edu>
2025-02-17 17:57:58 -0500

    Removed health endpoint:

--- routes/v1/health.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const healthController = require('../../controllers/health.controller')
-
-const router = express.Router()
-
-router.route('/').get(healthController.healthcheck)
-
-module.exports = router
--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,11 +1,9 @@
 const express = require('express')
-const healthRoute = require('./health.route')
 const helloWorldRoute = require('./helloworld.route')
 const agentRoute = require('./agent.route')
 
 const router = express.Router()
 
-router.use('/health', healthRoute)
 router.use('/', helloWorldRoute)
 router.use('/agent', agentRoute)
 
